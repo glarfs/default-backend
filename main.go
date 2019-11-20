6bc2a67ce826cc364ab3b3d1ea649484d521bcb3
Marc Rodriguez-Estivill <mrodriguezestivill@gmail.com>
2019-11-20 15:01:43 +0100

    added x-code parsing

--- main.go
+++ main.go
@@ -4,19 +4,33 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"fmt"
+	"strconv"
 )
 
 func main() {
 
 	// return 404 on / path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		w.WriteHeader(http.StatusNotFound)
+		xcode := r.Header.Get("X-Code")
+		if xcode == nil {
+			xcode := "404"
+		}
+		xcodeint, err = strconv.ParseInt(xcode)
+		if err != nil {
+			log.Error("Can't parse integer: " + xcode)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			w.WriteHeader(http.StatusNotFound)
+			return			
+		}
+		w.WriteHeader(xcodeint)
+		
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		file, err := ioutil.ReadFile("./assets/404.html")
+		file, err := ioutil.ReadFile("./assets/" + xcode + ".html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Fatal("Can't find error html page")
+			log.Fatal("Can't find error html page: " + xcode)
+			return
 		}
 		w.Write(file)
 	})
@@ -24,7 +38,7 @@ func main() {
 	// return 200 on healthz path
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("healthy!"))
+		w.Write([]byte("{\"status\":\"healthy!\"}"))
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
